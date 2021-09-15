6caffd4b9aee2f1707e1e4558c3db8a733fb44a8
Vector2588 <natkamol082@gmail.com>
2021-09-15 19:37:24 +0700

    1.9 search by lower case

--- src/main/java/se331/lab/rest/repository/EventRepository.java
+++ src/main/java/se331/lab/rest/repository/EventRepository.java
@@ -15,6 +15,6 @@ public interface EventRepository extends JpaRepository<Event,Long> {
     Page<Event> findByTitleContaining(String title, Pageable pageRequest);
     Page<Event> findByTitleContainingOrDescriptionContaining(String title, String description, Pageable pageRequest);
     Page<Event> findByTitleContainingAndDescriptionContaining(String title, String description, Pageable pageRequest);
-    Page<Event> findByTitleContainingOrDescriptionContainingOrOrganizer_NameContaining(String title, String description, String organizerName, Pageable pageRequest);
+    Page<Event> findByTitleIgnoreCaseContainingOrDescriptionIgnoreCaseContainingOrOrganizer_NameIgnoreCaseContaining(String title, String description, String organizerName, Pageable pageRequest);
 
 }
