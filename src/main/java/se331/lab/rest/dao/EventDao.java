e43fa7ef3aaaf3139635ec0369fc33daa2860bb7
Chartchai <chartchai.d@cmu.ac.th>
2021-09-15 16:44:54 +0700

    initial commit

---
+++ src/main/java/se331/lab/rest/dao/EventDao.java
@@ -0,0 +1,12 @@
+package se331.lab.rest.dao;
+
+import org.springframework.data.domain.Page;
+import se331.lab.rest.entity.Event;
+
+public interface EventDao {
+    Integer getEventSize();
+    Page<Event> getEvents(Integer pageSize, Integer page);
+    Event getEvent(Long id);
+
+    Event save(Event event);
+}
---
+++ src/main/java/se331/lab/rest/service/EventService.java
@@ -0,0 +1,12 @@
+package se331.lab.rest.service;
+
+import org.springframework.data.domain.Page;
+import se331.lab.rest.entity.Event;
+
+public interface EventService {
+    Integer getEventSize();
+    Page<Event> getEvents(Integer pageSize, Integer page);
+    Event getEvent(Long id);
+
+    Event save(Event event);
+}
